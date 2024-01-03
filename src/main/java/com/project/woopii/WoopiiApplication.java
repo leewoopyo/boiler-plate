c45a012a5d6084c87548a2d48ce8f0687112e3e2
Lstamp <lozenkrantz@gmail.com>
2024-01-04 01:49:36 +0900

    [ADD] init

---
+++ src/main/java/com/project/woopii/WoopiiApplication.java
@@ -0,0 +1,14 @@
+package com.project.woopii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WoopiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoopiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/woopii/WoopiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.woopii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoopiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
