96e90eb8c6686ddbbef1c1680bfc06b9e0ac9975
woopii <lozenkrantz@gmail.com>
2024-03-09 12:10:12 +0900

    [ADD] aws parameter store

--- src/main/java/com/project/woopii/WoopiiApplication.java
+++ src/main/java/com/project/woopii/WoopiiApplication.java
@@ -1,14 +1,29 @@
 package com.project.woopii;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
-public class WoopiiApplication {
+@Slf4j
+public class WoopiiApplication implements CommandLineRunner{
+
+    @Value("${application.name}")
+    private String message;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WoopiiApplication.class, args);
+
 	}
 
+	@Override
+    public void run(String... args) throws Exception {
+        log.info("Resolved message parameter: {}", message);
+
+    }
 }
